7427d5583eaa71126ae8dbd1d484cbd5ea3e79cc
Yaxing Chen <yangcongknight@gmail.com>
2017-05-06 21:13:33 +0000

    Set up nodejs loading data

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,4 +1,47 @@
-var controllers = angular.module('myapp.controllers', []);
-controllers.controller('IndexController', ['$scope', function($scope) {
-    $scope.message = 'Hello From Controller';
+var controllers = angular.module('dhtservice', ['ngAnimate', 'treasure-overlay-spinner']);
+
+controllers.controller('DataController', ['$scope', '$http', '$interval', 'dateFilter', function($scope, $http, $interval, dateFilter) {
+
+    $scope.data = {};
+
+    $scope.spinner = {
+        active: false,
+        on: function () {
+            this.active = true;
+        },
+        off: function () {
+            this.active = false;
+        }
+    };
+
+    $scope.spinner.on();
+
+    timer = $interval(function() {
+        $scope.time = dateFilter(new Date(), 'M/d/yy h:mm:ss a');
+    }, 1000);
+
+    loader = $interval(function() {
+        $http({
+            method : "GET",
+            url : "api/data"
+        }).then(function mySucces(response) {
+            $scope.data = response.data;
+            console.log($scope.data);
+            $scope.spinner.off();
+        }, function myError(response) {
+            $scope.error = response.data.message;
+            console.log($scope.error);
+        });
+    }, 1000);
+
+    $scope.$on('$destroy', function() {
+        if (angular.isDefined(loader)) {
+            $interval.cancel(loader);
+            loader = undefined;
+        }
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        }
+    });
 }]);
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var sensor = require('node-dht-sensor');
+var dateFormat = require('dateformat');
 
 /* GET home page. */
 router.get('/data', function(req, res, next) {
 	sensor.read(22, 4, function(err, temperature, humidity) {
         if (!err) {
             console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
-            res.json({ temp: temperature.toFixed(1), hum: humidity.toFixed(1), time: new Date().toLocaleString() });
+            res.json({ temp: temperature.toFixed(1), hum: humidity.toFixed(1), time: dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss TT") });
         }
         else {
             console.log(err);
