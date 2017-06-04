8e4ab56554b3ac5ff8a2cfd528251585b8436048
Yaxing Chen <yangcongknight@gmail.com>
2017-06-04 04:40:24 +0000

    clean up

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var index = require('./routes/index');
 var api = require('./routes/api');
 var stack = require('./routes/stack');
 
