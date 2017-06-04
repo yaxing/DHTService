7a1b58849eb2ea8ad0da1e82646a292789ffca1e
Yaxing Chen <yangcongknight@gmail.com>
2017-06-04 06:12:48 +0000

    Added cron to upload to MQTT baidu

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
+var Mqtt = require('mqtt');
+var sensor = require('node-dht-sensor');
+var dateFormat = require('dateformat');
 
 var api = require('./routes/api');
 var stack = require('./routes/stack');
@@ -43,4 +47,51 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var mqttParam = {
+    server: 'ssl://raspberry-pi.mqtt.iot.gz.baidubce.com:1884',
+    options: {
+        username: 'raspberry-pi/home',
+        password: 'El1S/J7cT0Z6ciIzluOtQXcNHZSKzMsnJpjUP0Hilkc=',
+        clientId: 'home-pi'
+    },
+    topic: 'r-pi-home/monitor-data'
+};
+
+var mqttClient = Mqtt.connect(mqttParam.server, mqttParam.options);
+console.log('Connecting to broker: ' + mqttParam.server);
+
+mqttClient.on('error', function(error) {
+    console.error(error);
+});
+
+mqttClient.on('message', function(topic, data) {
+    console.log('MQTT message received: ' + data);
+    if (data.toString() === 'exit') process.exit();
+});
+
+mqttClient.on('connect', function() {
+    console.log('Connected. Client id is: ' + mqttParam.options.clientId);
+    mqttClient.subscribe(mqttParam.topic);
+    console.log('Subscribed to topic: ' + mqttParam.topic)
+
+    upload();
+    var j = schedule.scheduleJob('* * * * *', function(){
+        upload();
+    });
+});
+
 module.exports = app;
+
+function upload() {
+    console.log("Reading Sensor and publishing data...");
+    sensor.read(22, 4, function(err, temperature, humidity) {
+        if (!err) {
+            var json = JSON.stringify({ temp: temperature.toFixed(1), hum: humidity.toFixed(1) });
+            mqttClient.publish(mqttParam.topic, json);
+            console.log("Published data: " + json);
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
